ab2e33f2da1b1a3624a062fa62e0632e2dd06331
bohua <fantasybohua@gmail.com>
2014-04-03 17:22:37 +0200

    Add code

--- models/rsda-master.js
+++ models/rsda-master.js
@@ -20,14 +20,14 @@ module.exports = function (sequelize) {
 		byxx:			{ type: Sequelize.STRING },
 		zy:				{ type: Sequelize.STRING },
 		bysj:			{ type: Sequelize.DATE },
-		ssbm:			{ type: Sequelize.INTEGER },
-		zw:				{ type: Sequelize.INTEGER },
+		//ssbm:			{ type: Sequelize.INTEGER },
+		//zw:				{ type: Sequelize.INTEGER },
 		sf:				{ type: Sequelize.STRING },
-		xzjb:			{ type: Sequelize.INTEGER },
+		//xzjb:			{ type: Sequelize.INTEGER },
 		gyxz:			{ type: Sequelize.STRING },
 		gl:				{ type: Sequelize.BIGINT },
 		bdwgl:			{ type: Sequelize.BIGINT },
-		zc:				{ type: Sequelize.INTEGER },
+		//zc:				{ type: Sequelize.INTEGER },
 		zc_date:		{ type: Sequelize.DATE },
 		cjgzdw_date:	{ type: Sequelize.DATE },
 		jbdw_date:		{ type: Sequelize.DATE },
@@ -55,7 +55,16 @@ module.exports = function (sequelize) {
 				RSDA_MASTER
 					.hasOne(models.RSDA_SOCIAL_SECURITY)
 					.hasOne(models.RSDA_FIRST_BIRTH)
-					.hasOne(models.RSDA_OTHER_INFO);
+					.hasOne(models.RSDA_OTHER_INFO)
+					.hasMany(models.RSDA_FAMILY_MEMBER)
+					.hasMany(models.RSDA_EXPERIENCE)
+					.hasMany(models.RSDA_SKILL)
+					.hasMany(models.RSDA_CERTIFICATE)
+					.belongsTo(models.REF_DEPARTMENT)
+					.belongsTo(models.REF_OCCUPATION)
+					.belongsTo(models.REF_PEOPLE_RACE)
+					.belongsTo(models.REF_ROLE_TITLE)
+					.belongsTo(models.REF_TIER_LEVEL);
 			}
 		}
 	});
